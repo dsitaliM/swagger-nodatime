be6ada05b39f556156c546ced23e8e5d376f2f11
Daniel Sitali <danielmsitali@outlook.com>
2022-03-22 18:23:35 +0200

    initial commit

---
+++ SwaggerNodaTime/Schema.fs
@@ -0,0 +1,67 @@
+namespace SwaggerNodaTime
+
+open System.Text.Json
+open Microsoft.OpenApi.Any
+open Microsoft.OpenApi.Models
+open NodaTime
+
+module Schemas =
+    type Container =
+        {
+            Instant: OpenApiSchema
+            LocalDate: OpenApiSchema
+            LocalTime: OpenApiSchema
+            LocalDateTime: OpenApiSchema
+            OffsetDateTime: OpenApiSchema
+            ZonedDateTime: OpenApiSchema
+            Interval: OpenApiSchema
+            Offset: OpenApiSchema
+            Period: OpenApiSchema
+            Duration: OpenApiSchema
+            DateTimeZone: OpenApiSchema
+        }
+    let Create (serializerSettings: JsonSerializerOptions): Container =
+        let stringRepresentation value =
+            let jsonString = JsonSerializer.Serialize(value, serializerSettings)
+            JsonSerializer.Deserialize<string>(jsonString)
+            
+        let stringSchema value =
+            OpenApiSchema(
+                Type = "object",
+                Example = OpenApiString(stringRepresentation value))
+
+        let instant = Instant.FromUtc(2016, 9, 22, 16, 53)
+        let duration = Duration.FromMilliseconds(49513784L)
+        let timeZone = DateTimeZoneProviders.Tzdb.["America/New_York"]
+
+        let zonedDateTime = instant.InZone(timeZone)
+        let localDateTime = zonedDateTime.LocalDateTime
+        let localDate = localDateTime.Date
+        let localTime = localDateTime.TimeOfDay
+        let offsetDateTime = zonedDateTime.ToOffsetDateTime()
+        let interval = Interval(instant, instant.Plus(duration))
+        let offset = timeZone.MaxOffset
+        let period = Period.Between(localDateTime, localDateTime.PlusTicks(duration.BclCompatibleTicks))
+
+        let intervalSchema =
+            let properties = System.Collections.Generic.Dictionary<string, OpenApiSchema>()
+            properties.Add("Start", stringSchema interval.Start)
+            properties.Add("End", stringSchema interval.End)
+
+            OpenApiSchema(
+                Type = "object",
+                Properties = properties)
+
+        {
+            Container.Instant = stringSchema instant
+            Container.LocalDate = stringSchema localDate
+            Container.LocalTime = stringSchema localTime
+            Container.LocalDateTime = stringSchema localDateTime
+            Container.OffsetDateTime = stringSchema offsetDateTime
+            Container.ZonedDateTime = stringSchema zonedDateTime
+            Container.Interval = intervalSchema
+            Container.Offset = stringSchema offset
+            Container.Period = stringSchema period
+            Container.Duration = stringSchema duration
+            Container.DateTimeZone = stringSchema timeZone
+        }
---
+++ SwaggerNodaTime/SwaggerExtensions.fs
@@ -0,0 +1,46 @@
+namespace SwaggerNodaTime
+
+
+open System.Runtime.CompilerServices
+open System.Text.Json
+open Microsoft.Extensions.DependencyInjection
+open NodaTime
+open Schemas
+open Swashbuckle.AspNetCore.SwaggerGen
+
+[<AutoOpen>]
+[<Extension>]
+module SwaggerExtensions =
+
+    [<Extension>]
+    let ConfigureForNodaTime (config: SwaggerGenOptions, serializerSettings: JsonSerializerOptions) =
+        let schemas =
+            serializerSettings
+            |> Schemas.Create
+
+        config.MapType<Instant>(fun () -> schemas.Instant)
+        config.MapType<LocalDate>(fun () -> schemas.LocalDate)
+        config.MapType<LocalTime>(fun () -> schemas.LocalTime)
+        config.MapType<LocalDateTime>(fun () -> schemas.LocalDateTime)
+        config.MapType<OffsetDateTime>(fun () -> schemas.OffsetDateTime)
+        config.MapType<ZonedDateTime>(fun () -> schemas.ZonedDateTime)
+        config.MapType<Interval>(fun () -> schemas.Interval)
+        config.MapType<Offset>(fun () -> schemas.Offset)
+        config.MapType<Period>(fun () -> schemas.Period)
+        config.MapType<Duration>(fun () -> schemas.Duration)
+        config.MapType<DateTimeZone>(fun () -> schemas.DateTimeZone)
+
+        config.MapType<System.Nullable<Instant>>(fun () -> schemas.Instant)
+        config.MapType<System.Nullable<LocalDate>>(fun () -> schemas.LocalDate)
+        config.MapType<System.Nullable<LocalTime>>(fun () -> schemas.LocalTime)
+        config.MapType<System.Nullable<LocalDateTime>>(fun () -> schemas.LocalDateTime)
+        config.MapType<System.Nullable<OffsetDateTime>>(fun () -> schemas.OffsetDateTime)
+        config.MapType<System.Nullable<ZonedDateTime>>(fun () -> schemas.ZonedDateTime)
+        config.MapType<System.Nullable<Interval>>(fun () -> schemas.Interval)
+        config.MapType<System.Nullable<Offset>>(fun () -> schemas.Offset)
+        config.MapType<System.Nullable<Duration>>(fun () -> schemas.Duration)
+
+    type SwaggerGenOptions with
+        member this.AddNodaTime(serializerSettings: JsonSerializerOptions) =
+            ConfigureForNodaTime(this, serializerSettings)
+    
